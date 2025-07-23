bd25f7f40f887719e26f81c5107cd7cb9399b3d2
Seunghwan-Ji <horong0602.dev@gmail.com>
2025-07-23 11:21:17 +0900

    25.07.23 수업 내용.

--- 32_Multi_Inheritance_01/main.cpp
+++ 32_Multi_Inheritance_01/main.cpp
@@ -4,66 +4,70 @@
 using namespace std;
 
 // 다중상속
-class Parent1
-{
+class Parent1 {
 public:
     int _value;
     int _value1;
 
 public:
     Parent1(int value, int value1)
-        : _value(value)
-        , _value1(value1)
-    {}
+        : _value(value), _value1(value1) {
+    }
 
-    void ShowInfo()
-    {
+    void ShowInfo() {
         cout << "Parent1:: _value = " << _value << ", _value1 = " << _value1 << endl;
     }
 
 };
 
-class Parent2
-{
+class Parent2 {
 public:
     int _value;
     int _value2;
 
 public:
     Parent2(int value, int value2)
-        : _value(value)
-        , _value2(value2)
-    {}
+        : _value(value), _value2(value2) {
+    }
 
-    void ShowInfo()
-    {
+    void ShowInfo() {
         cout << "Parent2:: _value = " << _value << ", _value2 = " << _value2 << endl;
     }
 };
 
 
-class Child : public Parent1, public Parent2
-{
+class Child : public Parent1, public Parent2 {
+private:
+    int _Cvalue = 0;
 public:
+
     Child(int valuep1, int value1, int valuep2, int value2)
-        : Parent1(valuep1, value1)
-        , Parent2(valuep2, value2)
-    {}
+        : Parent1(valuep1, value1), Parent2(valuep2, value2) {
+    }
+    /*
+    void ShowInfo() {
+       Parent1::ShowInfo();
+       Parent2::ShowInfo();
+    }
+    */
 };
 
 
-int main()
-{
+int main() {
     Child child(10, 20, 30, 40);
 
-    // child.ShowInfo(); 모호함이 발생하는 경우 정확히 호출 범위를 지정해주시면 됩니다.
-    child.Parent1::ShowInfo();
+    child.Parent1::ShowInfo(); // 모호함이 발생하는 경우 정확히 호출 범위를 지정해주시면 됩니다.
 
     child.Parent2::_value = 100;
 
-    Parent1& ref1 = child;
+    Parent1& ref1 = child; // UpCasting
 
     ref1.ShowInfo();
+
+    Parent2& ref2 = child;
+
+    ref2.ShowInfo();
+
 
     return 0;
 }
